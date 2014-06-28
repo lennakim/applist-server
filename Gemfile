84304b783a7f34019de8cfd20db6a2a2d9afd0d6
teng.cong <congteng45@gmail.com>
2014-06-28 11:42:20 +0800

    basic env

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://ruby.taobao.org'
+
+gem 'rails', '4.1.1'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+
+gem 'spring',        group: :development
+
+gem 'mongoid'
+gem 'bson_ext'
+
+gem 'settingslogic'
+gem 'inherited_resources'
+
+group :development, :test do
+  gem 'thin'
+  gem 'spring'
+  gem 'pry-rails'
+  gem 'pry-nav'
+  gem "quiet_assets"
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'rspec-rails'
+  gem 'database_cleaner'
+  gem 'awesome_print'
+  gem 'factory_girl_rails'
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  DatabaseCleaner.logger = Rails.logger
+
+end
+
